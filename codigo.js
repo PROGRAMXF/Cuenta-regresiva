a62a22912a58f3493e5716ced36e0e3c52149adb
Fernando <fh1273509@gmail.com>
2022-11-26 22:52:10 -0300

    codigo terminado

--- codigo.js
+++ codigo.js
@@ -1,6 +1,40 @@
-const $days = document.getElementById('days');
-const $hours = document.getElementById('hours');
-const $minutes = document.getElementById('minutes');
-const $seconds = document.getElementById('seconds');
-const $finalMessage = document.getElementById('.final-sms');
+const $days = document.getElementById('days'),
+ $hours = document.getElementById('hours'),
+ $minutes = document.getElementById('minutes'),
+ $seconds = document.getElementById('seconds'),
+ $finalMessage = document.querySelector('.final-sms');
+
+//fecha a futuro
+const countdownDate = new Date('Nov 30, 2022 22:51:00').getTime();
+
+//para que nuestros segundos se vayan actualizando cada milisegundo vamos a usar un setInterval
+let interval = setInterval(function(){
+    //obtener fecha actual y milisegundos
+    const now = new Date().getTime();
+
+//ahora vamos a obtener las distancias entre ambas fechas
+let distance = countdownDate - now;
+
+//calculos a dias, horas, minutos y segundos
+let days = Math.floor(distance / (1000 * 60 * 60 * 24))//nos da un valor en decimales
+//con Math.floor le sacamos los decimales
+
+let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+let seconds = Math.floor((distance % (1000 * 60)) / (1000));
+
+
+//escribimos los resultados
+$days.innerHTML = days;
+$hours.innerHTML = hours;
+$minutes.innerHTML = minutes;
+$seconds.innerHTML = ('0' + seconds).slice(-2);
+
+//cuando llege a 0
+if(distance < 0){
+    clearInterval(interval);
+    $finalMessage.style.transform = 'translateY(0)';
+}
+
+}, 1000);//se actualiza cada 1 segundo (1000 milisegundos es 1 segundo)
 
